be254184726465621dbb53a26315cb525463673d
Eric Jin <ericjin2@linux-a2.ews.illinois.edu>
2020-12-01 16:51:01 -0600

    Create Edge and LatLong Struct and Start Process File

--- graph.cpp
+++ graph.cpp
@@ -1,4 +1,5 @@
 #include "graph.h"
+#include <fstream> 
 
 Graph::Graph(string fileName)
 {
@@ -12,5 +13,14 @@ void Graph::translateDataToGraph(string fileName)
 
     //Key: string of the starting airport
     //Value: Vector of structs of the desintation airports along with data of the airline and equipment. 
-    
+
+    // Vector of edges of structs with start airport (IATA), end airport (IATA), alrine code
+
+    ifstream file(fileName);
+    string fileLine;
+
+    while (getline(file, fileLine)) {
+        // line contains the current line
+
+    }
 }
--- graph.h
+++ graph.h
@@ -1,10 +1,28 @@
 #include <string>
+#include <unordered_map> 
+
 using namespace std;
-class Graph
+class Graph 
 {
+
 public:
     Graph(string fileName);
     
 private:
+    class Edges 
+    {
+        string startAirport;
+        string endAirport;
+        string airlineCode;
+    };
+
+    struct LatLong 
+    {
+        double lat; // latitude
+        double lon; // longitude
+    };
+
     void translateDataToGraph(string fileName);
+
+    unordered_map<string, LatLong> airports;
 };
