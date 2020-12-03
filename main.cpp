54869911c6fa60ec352942384487c569494bcea6
Isaac Tong <isaact2@illinois.edu>
2020-12-02 23:18:39 -0600

    Created main.cpp, and empty Makefile

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "graph.h"
+#include <string>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+    string airportFile = "/dataset/airports_dataset.txt";
+    string routesFile = "/dataset/routes_dataset.txt";
+    Graph  newGraph(routesFile, airportFile);
+
+    newGraph.printAirports();
+    newGraph.printGraph();
+
+    return 0;
+}
